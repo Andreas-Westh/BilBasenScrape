e4b7c62e40dcc66d94b8fdf6f75a5d15597b1952
60gCreatine <cphah643@gmail.com>
2024-11-13 08:54:27 +0100

    fixed loop to not repeat first page

--- PageScrapeCode.R
+++ PageScrapeCode.R
@@ -2,7 +2,7 @@ library(httr)
 library(rvest)
 library(tidyverse)
 library(jsonlite)
-
+library(progress)
 
 
 # URL til bilbasen - søger efter BMW med diesel
@@ -53,13 +53,14 @@ ColnamesCars <- c("Pris", "Bilmodel", "Detaljer", "Beskrivelse", "Lokation", "Li
 carheader=c("pris","property","model","detailitems","description","location","link","carid","scrapedate")
 colnames(bilbasen100biler) <- ColnamesCars
 
-unique(bilbasen100biler$carid)
+pb <- progress_bar$new(total = 100)
+
 
 # Loop igennem biler og ekstraher data
 for (i in 1:30) {
   loopurl <- paste0(startlink,i)
-  Sys.sleep(3)
-  Loop <- paste0("Loopet har nu fanget: ",i*30," biler, ved loop: ",i,", klokken: ",Sys.time())
+  Sys.sleep(1:3)
+  Loop <- paste0("Loopet har nu fanget: ",i*30," biler, ved loop: ",i,", klokken: ",format(Sys.time(),"%a %b %d %X %Y"))
   print(Loop)
   rawres <- GET(
     url = loopurl,
@@ -71,7 +72,12 @@ for (i in 1:30) {
       `Accept` = "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
       `Cookie` = "bbsession=id=dcf43cd2-6536-47dc-8ff4-51d24110e759; bbtracker=id=4885ecaf-3275-4e5f-b55f-e3856aa2a855; _pulse2data=099e8942-dca8-4e0e-84e6-3512469bbe85%2Cv%2C%2C1731950624000%2CeyJpc3N1ZWRBdCI6IjIwMjQtMTEtMTFUMTc6MjM6MzhaIiwiZW5jIjoiQTEyOENCQy1IUzI1NiIsInJlSXNzdWVkQXQiOiIyMDI0LTExLTExVDE3OjIzOjQ0WiIsImFsZyI6ImRpciIsImtpZCI6IjIifQ..JnRCQnyJHjCCfa6moauXlg.QWh1TCV3dkx3dOorgjvSyGLlUGq1ry-WsJrlGqhHjLwYsfo2Iud_f9VCCJWJUc8SZ3vHK2FEFCxkuwhgxNhoS0h8dEeDaG46IyQRDgVwb3TTxGqR-jxCyObOCSHwjmX_UyQjy23zKUTQHR9xZWYZiexjHtJ8h…p_marketing=1; _cmp_advertising=1; consentUUID=2093de02-b803-4dfa-9a2b-6560e6650e4e_37; consentDate=2024-11-11T17:23:40.525Z; aws-waf-token=e18a363b-7943-4583-9347-97efcace04d7:CgoAvqI8VjhyAAAA:hbVS7hABFKoTeNwIIRjIM4fU/LbRNMz0vyKoCm0Ac5yGXTNfuu9UUf7jYtvrNaep6g9YbXsz/P2uqvKVKwdQLvuPofO/rxjLufdaACwHXb22kAO98ZpBLuou0KmwCdiLAfzEaZwK3ai5QfNRzg0gXDP3MxF/C4KoaHG2SrS0JI5TXA4FeESkUe2TFE3esjpF; _pulsesession=%5B%22sdrn%3Aschibsted%3Asession%3A69cd6bb4-5a21-4fa3-8c3b-45b5dcd03a40%22%2C1731401287643%2C1731402689664%5D"
     )
+    #pb$tick() progress bar, kan ikke få den til at virke
   )
+  rawcontent <- httr::content(rawres, as = "text", encoding = "UTF-8")
+  page <- read_html(rawcontent)
+  #### Loop ####
+  carlist <- page %>% html_elements("article")
   for (car in carlist) {
     tryCatch({
       pris <- car %>% html_element(ptag) %>% html_text(trim = TRUE)
@@ -93,6 +99,7 @@ for (i in 1:30) {
   }
 }
 
+unique(bilbasen100biler$carid) #tjekker antallet af unikke BilID
 
 
 
